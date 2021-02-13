2075dfb40be93b47bf12bfd91929e35fb2c450a4
Ian Mullett <ianmullett@gmail.com>
2021-02-13 17:51:04 -0500

    Section 1 complete

---
+++ Debug/CMakeFiles/OpenGL.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ian/School/Winter 2021/COMP 371/Udemy/src/main.cpp" "/home/ian/School/Winter 2021/COMP 371/Udemy/Debug/CMakeFiles/OpenGL.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "_deps/thirdparty-glew-src/include"
+  "../include"
+  "_deps/thirdparty-glfw-src/include"
+  "_deps/thirdparty-glm-src/glm/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ian/School/Winter 2021/COMP 371/Udemy/Debug/_deps/thirdparty-glfw-build/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Debug/_deps/thirdparty-glew-build/cmake_uninstall.cmake
@@ -0,0 +1,26 @@
+if(NOT EXISTS "/home/ian/School/Winter 2021/COMP 371/Udemy/Debug/_deps/thirdparty-glew-build/install_manifest.txt")
+  message(FATAL_ERROR "Cannot find install manifest: /home/ian/School/Winter 2021/COMP 371/Udemy/Debug/_deps/thirdparty-glew-build/install_manifest.txt")
+endif(NOT EXISTS "/home/ian/School/Winter 2021/COMP 371/Udemy/Debug/_deps/thirdparty-glew-build/install_manifest.txt")
+
+if (NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set (CMAKE_INSTALL_PREFIX "/home/ian/School/Winter 2021/COMP 371/Udemy/dist")
+endif ()
+ message(${CMAKE_INSTALL_PREFIX})
+
+file(READ "/home/ian/School/Winter 2021/COMP 371/Udemy/Debug/_deps/thirdparty-glew-build/install_manifest.txt" files)
+string(REGEX REPLACE "\n" ";" files "${files}")
+foreach(file ${files})
+  message(STATUS "Uninstalling $ENV{DESTDIR}${file}")
+  if(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+    exec_program(
+      "/usr/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+      OUTPUT_VARIABLE rm_out
+      RETURN_VALUE rm_retval
+      )
+    if(NOT "${rm_retval}" STREQUAL 0)
+      message(FATAL_ERROR "Problem when removing $ENV{DESTDIR}${file}")
+    endif(NOT "${rm_retval}" STREQUAL 0)
+  else(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+    message(STATUS "File $ENV{DESTDIR}${file} does not exist.")
+  endif(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+endforeach(file)
